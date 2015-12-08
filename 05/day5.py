c24a6ecce04ed9bff58075bf040ef26522fca412
Mica Swyers <micaswyers@gmail.com>
2015-12-07 21:09:45 -0800

    Solved part 2 of Day 5

--- 05/day5.py
+++ 05/day5.py
@@ -1,3 +1,5 @@
+import re
+
 # Part 1
 
 def is_nice(s):
@@ -45,3 +47,37 @@ def naughty_or_nice():
                 num_nice_strings += 1
     return num_nice_strings
 
+# Part 2
+def is_nice2(s):
+    """Determines if a string is naughty or nice based on bizarre criteria
+
+    Nice strings:
+        - contain a pair of any two letters that appears at least twice in the
+            string without overlapping
+            ex) xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+        - contain at least one letter which repeats with exactly one letter
+            between them
+            ex) xyx, abcdefeghi (efe), or even aaa.
+    Args:
+        s: some string
+    Returns:
+        Boolean (if string is nice)
+    """
+    pair_finder = re.compile('([a-z]{2})[a-z]*\\1')
+    repeat_finder = re.compile('([a-z])[a-z]\\1')
+
+    has_pair = bool(re.search(pair_finder, s))
+    has_repeat = bool(re.search(repeat_finder, s))
+
+    return has_pair and has_repeat
+
+
+def naughty_or_nice2():
+    """Returns number of nice strings based on new criteria"""
+
+    num_nice_strings = 0
+    with open('input.txt') as f:
+        for line in f:
+            if is_nice2(line):
+                num_nice_strings += 1
+    return num_nice_strings
