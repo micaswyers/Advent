24fa18e497e3722cc038d8f49a8ad6b5ffdf49b4
Mica Swyers <micaswyers@gmail.com>
2015-12-08 21:15:51 -0800

    Solved Part 2 of Day 6

--- 06/day6.py
+++ 06/day6.py
@@ -1,4 +1,7 @@
+# Part 1
 def parse_directions():
+    """Given the set of directions, return number of lights that are now on."""
+
     grid = define_matrix(1000)
     with open('input.txt') as f:
         for line in f:
@@ -42,3 +45,42 @@ def count_lights(grid):
                 on_lights += 1
 
     return on_lights
+
+# Part 2
+def follow_direction2(s, grid):
+    """Parse directions"""
+
+    s = s.split()
+    top_left = [int(x) for x in s[-3].split(',')]
+    bottom_right = [int(x) for x in s[-1].split(',')]
+
+    for yval, row in enumerate(grid[top_left[1]:(bottom_right[1]+1)], start=top_left[1]):
+        for xval, light in enumerate(row[top_left[0]:bottom_right[0]+1], start=top_left[0]):
+            if s[0] == "toggle":
+                row[xval] += 2
+            else:
+                if s[1] == "on":
+                    row[xval] += 1
+                elif s[1] == "off":
+                    if light == 0:
+                        continue
+                    row[xval] -= 1
+
+def count_brightness(grid):
+    """Returns total brightness of all illuminated lights in grid."""
+
+    total_brightness = 0
+    for row in grid:
+        for light in row:
+            total_brightness += light
+    return total_brightness
+
+
+def parse_directions2():
+    """Given the set of directions, return number of lights that are now on."""
+
+    grid = define_matrix(1000)
+    with open('input.txt') as f:
+        for line in f:
+            follow_direction2(line, grid)
+    return count_brightness(grid)
