c595c2779f51c66a5e236d0d4ee1a7189bdcf3b1
Mica Swyers <micaswyers@gmail.com>
2015-12-09 19:54:20 -0800

    Solved part 1 for day 7

--- 07/day7.py
+++ 07/day7.py
@@ -1,3 +1,4 @@
+
 # Part 1
 
 def save_directions():
@@ -7,24 +8,65 @@ def save_directions():
             wire_diagram.update([line])
     return wire_diagram
 
-def process_direction(direction):
-    directions = direction.split()
+def directions_loop(wire_diagram):
+    wire_values = {}
+    while 'a' not in wire_values:
+        for line in wire_diagram:
+            process_direction(line, wire_values)
+    print '*****'
+    print wire_values
+    return wire_values['a']
 
-    if len(directions) == 3:
-        # simple assignment
-        print 'assign'
-    elif len(directions) == 4:
-        print 'NOT'
-    elif len(directions) == 5:
+def process_direction(direction, wire_values):
+    operation = direction.split(' -> ')
+
+    directions = operation[0].split()
+    assigned = operation[1].strip()
+
+    if len(directions) == 1:
+        op1 = get_value(directions[0], wire_values)
+        if op1 != -1:
+            print "assigning %s to dict: %s" % (op1, assigned)
+            wire_values[assigned] = op1 & 0xffff
+    elif len(directions) == 2:
+        op1 = get_value(directions[1], wire_values)
+        if op1 != -1:
+            print "assigning %s to dict: %s" % (~op1, assigned)
+            wire_values[assigned] = ~op1 & 0xffff
+    elif len(directions) == 3:
         if directions[1] == 'AND':
-            print 'AND' # and code
+            op1 = get_value(directions[0], wire_values)
+            op2 = get_value(directions[2], wire_values)
+            if op1 != -1 and op2 != -1:
+                print "assigning %s to dict: %s" % (op1 & op2, assigned)
+                wire_values[assigned] = (op1 & op2) & 0xffff
         elif directions[1] == 'OR':
-            print 'OR' # or code
+            op1 = get_value(directions[0], wire_values)
+            op2 = get_value(directions[2], wire_values)
+            if op1 != -1 and op2 != -1:
+                print "assigning %s to dict: %s" % (op1 | op2, assigned)
+                wire_values[assigned] = (op1 | op2) & 0xffff
         elif directions[1] == 'LSHIFT':
-            print 'LSHIFT' # left-shift code
+            op1 = get_value(directions[0], wire_values)
+            op2 = get_value(directions[2], wire_values)
+            if op1 != -1 and op2 != -1:
+                print "assigning %s to dict: %s" % (op1 << op2, assigned)
+                wire_values[assigned] = (op1 << op2) & 0xffff
         elif directions[1] == 'RSHIFT':
-            print 'RSHIFT' # right-shift code
+            op1 = get_value(directions[0], wire_values)
+            op2 = get_value(directions[2], wire_values)
+            if op1 != -1 and op2 != -1:
+                print "assigning %s to dict: %s" % (op1 >> op2, assigned)
+                wire_values[assigned] = (op1 >> op2) & 0xffff
+    return
 
+def get_value(s, d):
+    """Determine if string s is in d or if it's a literal"""
 
-
-
+    if s in d:
+        return d[s]
+    try:
+        return int(s)
+    except Exception:
+        return -1
+    return -1
