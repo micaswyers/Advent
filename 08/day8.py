06980c3d101037cad04381821d061997e0037df6
Mica Swyers <micaswyers@gmail.com>
2015-12-10 20:48:37 -0800

    Make Part 1 solution not look like gross garbage

--- 08/day8.py
+++ 08/day8.py
@@ -1,23 +1,10 @@
-def count_code_chars(raw_string):
-    """Given raw_string, return the number of characters of CODE that it requires."""
-    count = len(raw_string)
-    return count
-
+# Part 1
 def total_chars():
 
-    return total_code_chars() - total_printed_chars()
-
-
-def total_code_chars():
-    code_chars = 0
     with open('input.txt') as f:
-        for raw_string in f:
-            code_chars += count_code_chars(raw_string.strip())
-    return code_chars
-
-def total_printed_chars():
-    chars = 0
-    with open('input.txt') as f:
+        code_chars = 0
+        chars = 0
         for line in f:
+            code_chars += len(line.strip())
             chars += len(eval(line.strip()))
-    return chars
+        return code_chars - chars
