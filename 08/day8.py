ae1b0c710c1a54e7f2cc63ef04f1e830f250c14f
Mica Swyers <micaswyers@gmail.com>
2015-12-10 21:28:14 -0800

    Solved Part 2 of Day 8

--- 08/day8.py
+++ 08/day8.py
@@ -1,5 +1,6 @@
 # Part 1
 def total_chars():
+    """Given a set of strings, return the difference between # of code chars & chars"""
 
     with open('input.txt') as f:
         code_chars = 0
@@ -8,3 +9,17 @@ def total_chars():
             code_chars += len(line.strip())
             chars += len(eval(line.strip()))
         return code_chars - chars
+
+# Part 2
+def total_chars2():
+    """Escape all the escapes!"""
+
+    chars_count = 0
+    code_chars = 0
+    with open('input.txt') as f:
+        for line in f:
+            code_chars += len(line.strip())
+            line = line.replace("\\", r"\\")
+            line = line.replace("\"", r'\"')
+            chars_count += (len(line.strip()) + 2)
+    return chars_count - code_chars
