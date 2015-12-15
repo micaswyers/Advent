1862353af971a3f4d8017be36254b5176d333540
Mica Swyers <micaswyers@gmail.com>
2015-12-14 21:49:31 -0800

    WIP

--- 14/day14.py
+++ 14/day14.py
@@ -13,5 +13,40 @@ def parse_input():
             reindeer_dict[name] = (fly_speed, fly_duration, rest_duration)
     return reindeer_dict
 
-def simulate_race():
+def simulate_reindeer(race_time, fly_speed, fly_duration, rest_duration):
 
+    cycle_length = fly_duration + rest_duration
+    num_cycles = race_time/cycle_length
+    distance = num_cycles * fly_speed * fly_duration
+    extra_time = race_time % cycle_length
+    if extra_time <= fly_duration:
+        distance += (fly_speed * extra_time)
+    else:
+        distance += (fly_speed * fly_duration)
+
+    return distance
+
+def simulate_race(race_time):
+    """Returns name & distance of winning reindeer."""
+
+    reindeer_dict = parse_input()
+    distances = {}
+    for reindeer, reindeer_stats in reindeer_dict.iteritems():
+        fly_speed, fly_duration, rest_duration = reindeer_stats
+        distance = simulate_reindeer(race_time, fly_speed, fly_duration, rest_duration)
+        distances[reindeer] = distance
+    return max(distances.items(), key=lambda x:x[1])
+
+
+# Part 2
+def simulate_race2(race_time):
+    """Returns number of points of winning reindeer."""
+
+    reindeer_stats_dict = parse_input()
+    points = defaultdict(lambda: 0)
+    lead = None
+
+    for i in range(1, race_time+1):
+        lead, distance = simulate_race(i)
+        points[lead] += 1
+    return max(points.items(), key=lambda x:x[1])
