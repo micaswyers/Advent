da410a104db830196b38a35a7f82a99d3c045248
Mica Swyers <micaswyers@gmail.com>
2015-12-17 21:42:44 -0800

    Solution to Part 2 Day 17

--- 17/day17.py
+++ 17/day17.py
@@ -36,3 +36,39 @@ def find_combinations():
     num_combos = decant_nog(bottles_list)
     return num_combos
 
+# Part 2
+def decant_nog2(bottles_list, depth, nog_volume=150):
+    """Finds number of combinations of bottles in bottles_list where number of bottles used equals depth
+
+        Args:
+            bottles_list: list of ints, each representing bottle volume
+            depth: number of times (int) that the function is allowed to be called
+            nog_volume: int representing num liters of egg nog to decant
+        Returns:
+            num_combos: number of possible combinations (int)
+    """
+
+    num_combos = 0
+    if not nog_volume:
+        return 1
+    elif nog_volume < 0:
+        return 0
+    if depth == 0:
+        return 0
+    for idx in range(len(bottles_list)):
+        new_vol = nog_volume - bottles_list[idx]
+        num_combos += decant_nog2(bottles_list[idx+1:], depth-1, new_vol)
+    return num_combos
+
+def find_combinations2():
+    """Returns number of combinations for the minimum number of bottles to decant nog
+
+        (Also prints the number of bottles)
+    """
+
+    bottles_list = parse_input()
+    for idx in range(len(bottles_list)):
+        num_combos = decant_nog2(bottles_list, idx+1)
+        if num_combos != 0:
+            print idx + 1
+            return num_combos
