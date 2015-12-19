913c478b88c4cad1022c0e227c692392e056b5ce
Mica Swyers <micaswyers@gmail.com>
2015-12-18 21:32:26 -0800

    Add docstrings & solution to Part 2 for Day 18

--- 18/day18.py
+++ 18/day18.py
@@ -1,6 +1,14 @@
 # Part 1
-def define_grid(n=100, f="input.txt"):
-    """Given an input file, recreates it as a nxn matrix"""
+def define_grid(n=100, f="input.txt", stuck_corners=False):
+    """Given an input file, recreates it as a nxn matrix
+
+        Args:
+            n: int, dimension for a square matrix
+            f: string, name of input file
+            stuck_corners: bool, indicates whether four corner lights should always be on
+        Returns:
+            grid: nxn matrix recreating input characters in f-file
+    """
 
     grid = []
     with open(f) as input_file:
@@ -9,10 +17,23 @@ def define_grid(n=100, f="input.txt"):
             for char in line.strip():
                 row.append(char)
             grid.append(row)
+    if stuck_corners:
+       return turn_on_corners(grid)
     return grid
 
 def should_turn_on(point, grid):
-    # point is a tuple (i, j)
+    """Returns True if a light should be turned on based on surrounding points
+        - Each light's next state depends on the surrounding 8 lights' states.
+        - Edge & corner lights have fewer than 8 neighbors; those are considered OFF.
+        - An on-light with 2 or 3 surrounding on lights stays on.
+        - An off-light with exactly 3 surrounding on lights turns on.
+        - Everything else is turned off.
+        Args:
+            point: tuple, containing the row & col for a given light (both ints)
+            grid: an nxn matrix
+        Returns:
+            - True if the light should turn on; False otherwise
+    """
 
     row = point[0]
     col = point[1]
@@ -45,6 +66,7 @@ def should_turn_on(point, grid):
         return False
 
 def animate_grid_once(grid):
+    """Given a grid, animate it once"""
 
     next_grid = [["." for x in range(len(grid))] for x in range(len(grid))]
     for i, row in enumerate(grid):
@@ -55,12 +77,21 @@ def animate_grid_once(grid):
     return next_grid
 
 def animate_grid_multiple(grid, num_iterations=100):
+    """Animate the grid n-number of times
+
+        Args:
+            grid: some nxn matrix
+            num_iterations: the number of times to run the animation step
+        Returns:
+            grid: The updated grid based on n iterations of animation
+    """
 
     for iteration in range(num_iterations):
         grid = animate_grid_once(grid)
     return grid
 
 def count_on_lights(grid):
+    """Given some grid, return the number of illuminated lights."""
 
     on_count = 0
     for i, row in enumerate(grid):
@@ -69,4 +100,22 @@ def count_on_lights(grid):
                 on_count += 1
     return on_count
 
+# Part 2
 
+def turn_on_corners(grid):
+    """Turns all four corners of grid to "#" (On)"""
+
+    x = len(grid) - 1
+    grid[0][0] = "#"
+    grid[0][x] = "#"
+    grid[x][0] = "#"
+    grid[x][x] = "#"
+    return grid
+
+def animate_grid_with_on_corners(grid, num_iterations=100):
+    """Given a grid, animate it once but corner four lights are stuck on"""
+
+    for iteration in range(num_iterations):
+    # Make sure lights are stuck on in the corners
+        grid = turn_on_corners(animate_grid_once(grid))
+    return grid
