56ac36eef5fb64ec43964553dad6d062494a0bf0
Mica Swyers <micaswyers@gmail.com>
2015-12-18 21:10:18 -0800

    Solution to Part 1 Day 18

--- 18/day18.py
+++ 18/day18.py
@@ -16,7 +16,6 @@ def should_turn_on(point, grid):
 
     row = point[0]
     col = point[1]
-    print "Light in question is (%s,%s) = %s" % (row, col, grid[row][col])
     lights_on_count = 0
     last_row = row - 1
     next_row = row + 1
@@ -31,43 +30,43 @@ def should_turn_on(point, grid):
         last_col = col
     if next_col > len(grid) - 1:
         next_col = col
-    print "last_row ", last_row
-    print "next_row ", next_row
-    print "last_col ", last_col
-    print "next_col ", next_col
     for i in range(last_row, next_row+1):
         for j in range(last_col, next_col+1):
-            print "Looking at light: [%s,%s] = %s" % (i,j, grid[i][j])
             if grid[i][j] == "#":
                 lights_on_count += 1
     if grid[row][col] == "#":
-        print "Lights_On_count = ", lights_on_count
         lights_on_count -= 1
         if lights_on_count == 2 or lights_on_count == 3:
-            print "Need to leave ON light on"
             return True
         return False
     elif grid[row][col] == ".":
-        print "Lights_On_count = ", lights_on_count
         if lights_on_count == 3:
-            print "need to turn OFF light on"
             return True
         return False
 
-def animate_grid(grid):
+def animate_grid_once(grid):
 
     next_grid = [["." for x in range(len(grid))] for x in range(len(grid))]
     for i, row in enumerate(grid):
-        print "Row: ", i
         for j, col in enumerate(row):
             on = should_turn_on((i,j), grid)
             if on:
                 next_grid[i][j] = "#"
     return next_grid
 
+def animate_grid_multiple(grid, num_iterations=100):
 
-# For each line in the grid
-    # animate each line
-        # keep track of previous line, current line, next line to determine if should be on or off
-        # will need to append to a new grid
-# When all lines have been animated the right number of times, count lights
+    for iteration in range(num_iterations):
+        grid = animate_grid_once(grid)
+    return grid
+
+def count_on_lights(grid):
+
+    on_count = 0
+    for i, row in enumerate(grid):
+        for light in row:
+            if light == "#":
+                on_count += 1
+    return on_count
+
+
