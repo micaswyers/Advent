c350125dacc751f293b9772aebe80556e7b54568
Mica Swyers <micaswyers@gmail.com>
2015-12-22 21:54:33 -0800

    Solution to Day 20 - Part 1

--- 20/day20.py
+++ 20/day20.py
@@ -1,22 +1,29 @@
+from math import sqrt
 # Part 1
 
-def count_presents(input_gifts):
+def find_house(num_gifts):
+    """Given a number of gifts, return the first house that gets at least that many.
 
-    is_found = False
-    house_num = 0
-    while not is_found:
-        house_num += 1
-        total = 0
-        for idx in range(1,2):
-            if not house_num % idx:
-                num_presents = idx * 10
-                print "House %s gets %s presents" % (house_num, num_presents)
-                total += num_presents 
-            if total == input_gifts:
-                in_found = True
-    return total
+        Args:
+            num_gifts: int
+        Returns:
+            house: Number of house receiving at least as many gifts as num_gifts
+    """
+    house = 0
+    gifts = 0
+    while gifts < num_gifts:
+        house += 1
+        elves = find_factors(house)
+        gifts = sum(elves) * 10
+    return house
 
+def find_factors(num):
+    """Given num (int), return its factors, that we are calling elves."""
 
-
-
-
+    factors = set()
+    x = int(sqrt(num))
+    for idx in range(1, x + 1):
+        if not num % idx:
+            factors.add(idx)
+            factors.add(num/idx)
+    return factors
