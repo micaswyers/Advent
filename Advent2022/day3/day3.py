cd75847de5d4d9b6358c1b282ec55bde37feeb75
Mica Swyers <mica@mica.is>
2022-12-07 19:05:22 +0000

    Extremely ugly answer to day3 part 2

--- Advent2022/day3/day3.py
+++ Advent2022/day3/day3.py
@@ -18,7 +18,6 @@ def sum_priorities(filename='input.txt'):
             overlap = list(c1.intersection(c2))
             priorities.append(overlap[0])
     lower = _create_priority_dicts('lower')
-    print(lower)
     upper = _create_priority_dicts('upper')
 
     total = 0
@@ -29,5 +28,33 @@ def sum_priorities(filename='input.txt'):
             total += upper[p]
     return total
 
+# Part 2
+def sum_badges(filename="input.txt"):
+    lower = _create_priority_dicts('lower')
+    upper = _create_priority_dicts('upper')
 
-print(sum_priorities())
+    with open(filename, 'r') as f:
+        rucksacks = []
+        for line in f:
+            rucksacks.append(line.strip())
+        groups = list(zip(*(iter(rucksacks),) * 3))
+
+    badges = []
+    for group in groups:
+        badge = set(group[0]).intersection(set(group[1]).intersection(set(group[2])))
+        badges.append(badge)
+    
+    total = 0
+    for badge in badges:
+        token = str(badge)[2]
+        if token.islower():
+            total += lower[token]
+        elif token.isupper():
+            total += upper[token]
+    return total
+
+
+
+
+# print(sum_priorities())
+print(sum_badges())
